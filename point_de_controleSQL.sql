c6ee013a34761bac4e1e683087741d7b977d40fa
Lamineg1 <glamine98@icloud.com>
2023-11-16 11:02:09 +0000

    point de controle

--- point_de_controleSQL.sql
+++ point_de_controleSQL.sql
@@ -21,5 +21,24 @@ ADD CONSTRAINT const_i FOREIGN KEY (customer_id) REFERENCES customer (costumer_i
 ALTER TABLE commandes
 ADD CONSTRAINT const_i FOREIGN KEY (product_id) REFERENCES product (product_id);
 
+INSERT INTO produits values (  1 ,'cookie',  '10')
+
+INSERT INTO produits values ( 2, 'candy',  '5,2')
 
 
+INSERT INTO clients values ( 1, 'ahmed',  'tunisie')
+INSERT INTO clients values ( 2, 'coulibaly',  'senegale')
+INSERT INTO clients values ( 3, 'hasan',  'egypte')
+
+INSERT INTO commandes values ( 1, 2, 3 , '2023-01-22')
+INSERT INTO commandes values ( 1, 2, 10, '2023-04-14')
+
+
+UPDATE commandes 
+SET  customer_id= 6
+WHERE order_id = 2;
+
+DELETE FROM clients WHERE customer_id = 3
+
+
+DELETE commandes
